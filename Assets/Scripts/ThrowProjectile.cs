ead1c175da0f806f6dc7210d424894ace52c0109
scanim-valentin <valentin.guerbette@hotmail.fr>
2022-02-17 20:46:40 +0100

    added pie model

--- Assets/Scripts/ThrowProjectile.cs
+++ Assets/Scripts/ThrowProjectile.cs
@@ -8,7 +8,7 @@ public class ThrowProjectile : MonoBehaviour
     public GameObject projectile; 
     public GameObject thrower;
     public GameObject HUD;  
-
+    private Vector3 throwForce ; 
     private enum throwingMode{
         FLAT, FRIZBEE
     }
@@ -23,15 +23,19 @@ public class ThrowProjectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(currentThrowingMode == throwingMode.FLAT)
-            projectile.transform.rotation = Quaternion.Euler(0, 0, 90) ; 
-        if(currentThrowingMode == throwingMode.FRIZBEE)
+        if(currentThrowingMode == throwingMode.FRIZBEE){
+            projectile.transform.rotation = Quaternion.Euler(-90, 0, 0) ;
+            throwForce = Vector3.forward * 30 ; 
+        } 
+        if(currentThrowingMode == throwingMode.FLAT){
             projectile.transform.rotation = Quaternion.Euler(0, 0, 0) ; 
+            throwForce = Vector3.forward * 10 ; 
+        }
 
         if(Input.GetMouseButtonDown(0)){
             GameObject projectileInstance = Instantiate(projectile, thrower.transform.position, projectile.transform.rotation) ;
             projectileInstance.SetActive(true) ; 
-            projectileInstance.GetComponent<Rigidbody>().AddForce(Vector3.forward * 10, ForceMode.Impulse) ; 
+            projectileInstance.GetComponent<Rigidbody>().AddForce(throwForce, ForceMode.Impulse) ; 
         }
 
         if(Input.GetMouseButtonDown(1)){

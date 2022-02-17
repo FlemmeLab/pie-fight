4452303989fc03f1776e8b159f615fb67e5c4927
scanim-valentin <valentin.guerbette@hotmail.fr>
2022-02-17 17:01:57 +0100

    thrower, projectile rotation

--- Assets/Scripts/ThrowProjectile.cs
+++ Assets/Scripts/ThrowProjectile.cs
@@ -6,6 +6,7 @@ public class ThrowProjectile : MonoBehaviour
 {
     
     public GameObject projectile; 
+    public GameObject thrower;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +18,7 @@ public class ThrowProjectile : MonoBehaviour
     void Update()
     {
          if(Input.GetMouseButtonDown(0)){
-            GameObject projectileInstance = Instantiate(projectile, transform.position, transform.rotation) ;
+            GameObject projectileInstance = Instantiate(projectile, thrower.transform.position, projectile.transform.rotation) ;
             projectileInstance.SetActive(true) ; 
             projectileInstance.GetComponent<Rigidbody>().AddForce(Vector3.forward * 10 , ForceMode.Impulse) ; 
         }
